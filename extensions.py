1f0479262d8564bad665785dea4d20a0aaf8a8d8
Edmar <ramdepereiradossantos@gmail.com>
2026-08-29 22:50:03 -0300

    fix: ativa foreign keys nas conexoes sqlite

--- extensions.py
+++ extensions.py
@@ -1,6 +1,20 @@
+import sqlite3
+
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+
+
+@event.listens_for(Engine, "connect")
+def ativar_foreign_keys_sqlite(conexao_dbapi, _registro_conexao):
+    if not isinstance(conexao_dbapi, sqlite3.Connection):
+        return
+
+    cursor = conexao_dbapi.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
+    cursor.close()
 
 
 db = SQLAlchemy()
